796f549dd4944172f110f8f4ef130ea7e4d88652
Osi <osachi752@gmail.com>
2025-02-27 19:44:02 -0500

    Part 1: Growing Pains

---
+++ index.js
@@ -0,0 +1,20 @@
+/**
+ * Part 1: Growing Pains
+ */
+
+console.log(`\nPart 1: Growing Pains`);
+
+const groundRadius_m = 5;
+const PI = 3.1415;
+const groundArea_m = PI * radius * radius;
+const minPlantSpace_m = 0.8;
+const startingPlants = 20;
+const currentPlantSpace_m = minPlantSpace_m * startingPlants;
+
+// Predict the plant growth after a specific number of weeks.
+let plantGrowth1 = startingPlants * (2 * 1);
+let plantGrowth2 = startingPlants * (2 * 2);
+let plantGrowth3 = startingPlants * (2 * 3);
+
+
+if (plantGrowth1 > )
